cdd335e5e167c6d3dfac9f4e040d08c2078151f0
Takayuki Hirayama <hirayama@fiilse.com>
2016-09-09 12:34:35 +0900

    base added

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,4 +1,17 @@
 class ReportsController < ApplicationController
   def index
+		require "date"
+		youbi = %w[日 月 火 水 木 金 土]
+		d = Date.today
+		@youbi = youbi[d.wday]
+		@end_time = Time.now.strftime('%H').to_i
+		@start_time = 10
+		@work_time = @end_time - @start_time
+		@working_time = @work_time - 1
+		@today = Time.now.strftime('%Y-%m-%d')
+		@end   = Time.now.strftime('%H')
+		@time  = '10:00-' + @end + ':00' 
+
+
   end
 end
