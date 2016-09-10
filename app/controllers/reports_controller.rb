5128fd73f601fe25e5655f6dd49e769693989681
shoday10 <imada@fiilse.com>
2016-09-10 23:09:45 +0900

    時間,実働時間を30分区切りに変更

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -4,18 +4,21 @@ class ReportsController < ApplicationController
 		youbi = %w[日 月 火 水 木 金 土]
 		d = Date.today
 		@youbi = youbi[d.wday]
-		@end_time = Time.now.strftime('%H').to_i
+		@end_hour = Time.now.strftime('%H').to_i
+		@end_minit = Time.now.strftime('%M').to_i
 		@start_time = 10
-		@work_time = @end_time - @start_time
-		@working_time = @work_time - 1
 		@today = Time.now.strftime('%Y-%m-%d')
 		@end   = Time.now.strftime('%H')
 		@end_minit = Time.now.strftime('%M').to_i
-		if @end_minit.to_i > 30
+		#30分区切りにする
+		if @end_minit > 30
 			@time  = '10:00-' + @end + ':30'
+			@work_time = @end_hour - @start_time + 0.5
 		else
 			@time  = '10:00-' + @end + ':00'
+			@work_time = @end_hour - @start_time
 		end
+		@working_time = @work_time - 1
 		#@report = Report.find_by(id: 1).body
 	end
 
